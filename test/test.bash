63d845b0448b1990c122feae7b6620b126efec73
Yuya Koyama <vulpes.yuya@gmail.com>
2023-11-23 14:54:58 +0900

    add test.yml

--- test/test.bash
+++ test/test.bash
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-dir=-
-[ "$1" !=  "" ] && dir="$1"
+dir=~
+[ "$1" != "" ] && dir="$1"
 
 cd $dir/ros2_ws
 colcon build
-sourse $dir/.bashrc
+source $dir/.bashrc
 timeout 10 ros2 launch mypkg talk_listen.launch.py > /tmp/mypkg.log
 
 cat /tmp/mypkg.log |
